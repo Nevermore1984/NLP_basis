97a8c3106d2cc2bd6b9aa4e600116820300b5763
Nevermore1984 <15962976606@163.com>
2019-08-07 16:04:46 +0800

    little commit

--- 3_chapter/3.3准确分词之动态调整词频和字典顺序/cut_data.py
+++ 3_chapter/3.3准确分词之动态调整词频和字典顺序/cut_data.py
@@ -1,30 +1,37 @@
 #-*- coding=utf8 -*-
 import jieba
 import re
-from tokenizer import cut_hanlp
+#from tokenizer import cut_hanlp
 
 
-#jieba.load_userdict("dict.txt")
+jieba.load_userdict("dict.txt")
 
 # # 设置高词频：一个
 # jieba.suggest_freq('台中',tune=True)
 
 # 设置高词频：dict.txt中的每一行都设置一下
-# fp=open("dict.txt",'r',encoding='utf8')
-# for line in fp:
-#     line=line.strip()
-#     jieba.suggest_freq(line, tune=True)
+fp=open("dict.txt",'r',encoding='utf8')
+for line in fp:
+    line=line.strip()
+    jieba.suggest_freq(line, tune=True)
+if __name__=="__main__":
+    string="台中正确应该不会被切开。"
+
+    words=jieba.cut(string,HMM=False)
+
+    result=" ".join(words)
+    print(result)
 
 # # 设置高词频：dict.txt中的每一行都设置一下快速方法
 # [jieba.suggest_freq(line.strip(), tune=True) for line in open("dict.txt",'r',encoding='utf8')]
 
 
-if __name__=="__main__":
-    string="台中正确应该不会被切开。"
+#if __name__=="__main__":
+ #   string="台中正确应该不会被切开。"
 
-    words_jieba = " ".join(jieba.cut(string,HMM=False))
+  #  words_jieba = " ".join(jieba.cut(string,HMM=False))
 
-    words_hanlp=cut_hanlp(string)
-    print("words_jieba:"+words_jieba,'\n',"words_hanlp:"+words_hanlp)
+   # words_hanlp=cut_hanlp(string)
+    #print("words_jieba:"+words_jieba,'\n',"words_hanlp:"+words_hanlp)
     
   
